28aa616b0ae5347d5f6a066ab46472fbfb395fbe
scrowland <simon.rowland@fetch.ai>
2025-01-28 12:05:28 +0000

    feat(examples): move base tests to enable disambiguation

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="fetchai",
     version="0.1.18",
-    packages=find_packages(exclude=("tests", "examples")),
+    packages=find_packages(exclude=("fetchai/tests", "examples")),
     install_requires=[
         "bech32>=1.2.0,<2.0",
         "ecdsa>=0.19.0,<1.0",
