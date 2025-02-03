c532cfe93a7f138a502cdde7232af0a17537a9b1
scrowland <simon.rowland@fetch.ai>
2025-02-03 11:11:35 +0000

    feat(examples): tidy up code and make all data generic

--- examples/readme-agent/readme_agent/register_agent.py
+++ examples/readme-agent/readme_agent/register_agent.py
@@ -7,22 +7,32 @@ from fetchai.registration import register_with_agentverse
 
 
 load_dotenv()
+
 # Your Agentverse API Key for utilizing webtools on your AI that is
 # registered in the AI Alliance Almanac.
 AGENTVERSE_KEY = os.getenv("AGENTVERSE_KEY")
+
+# A unique seed to use for your Identity creation - do not use the
+# defaults.
 MY_AI_KEY = os.getenv("MY_AI_KEY")
+
+# The local url your agent is running at
+MY_WEBHOOK_URL = os.getenv("MY_WEBHOOK_URL")
+
+
+if "Generate a unique string" in MY_AI_KEY:
+    raise ValueError("Do not use the default value for MY_AI_KEY - Generate a unique string in your .env file")
 
 
 def main():
     # Your AI's unique key for generating an address on agentverse
     ai_identity = Identity.from_seed(MY_AI_KEY, 0)
-    print("Got ai identity")
 
-    # Give your AI a name on agentverse. This allows you to easily identify one
+    # Give your AI a name on Agentverse. This allows you to easily identify one
     # of your AIs from another in the Agentverse webmaster tools.
-    name = "SimonTestFetchAIOne"
+    name = "Example Agent"
 
-    # This is how you optimize your AI's search engine performance
+    # Use realistic details in your README description to enable discovery of your agent
     readme = """
     <description>My AI's description of capabilities and offerings</description>
     <use_cases>
@@ -39,18 +49,14 @@ def main():
     </payload_requirements>
     """
 
-    # The webhook that your AI receives messages on.
-    ai_webhook = "https://simon.rowland.ngrok.dev/webhook"
-
     print("registering with agentverse")
     result = register_with_agentverse(
         ai_identity,
-        ai_webhook,
+        MY_WEBHOOK_URL,
         AGENTVERSE_KEY,
         name,
         readme,
     )
-
     print("Registration result: ", result)
 
 
--- examples/readme-agent/readme_agent/send_message.py
+++ examples/readme-agent/readme_agent/send_message.py
@@ -3,41 +3,36 @@ import os
 from dotenv import load_dotenv
 from fetchai import fetch
 from fetchai.crypto import Identity
-from fetchai.communication import (
-    send_message_to_agent
-)
+from fetchai.communication import send_message_to_agent
 
 
 load_dotenv()
 
-AGENT_ADDRESS = "agent1q2rkmh7a7fka48mf5rp9pt37n2fccppvwlcfjtlpeankh02nhae0vy7nffs"
+AGENT_ADDRESS = "<Add the Agentverse Address of your registered agent here>"
 MY_AI_KEY = os.getenv("MY_AI_KEY")
+
 
 def main():
     query = "Buy me a pair of shoes"
-    available_ais = fetch.ai(query)
 
     # This is our AI's personal identity, it's how
     # the AI we're contacting can find out how to
     # get back a hold of our AI.
-    # See the "Register Your AI" section for full details.
+    # This must match the Identity created when your agent was registered
     sender_identity = Identity.from_seed(os.getenv("MY_AI_KEY"), 0)
 
-    # We'll make up a payload here but you should
+    # We'll make up a payload here, but you should
     # use the readme provided by the other AIs to have
     # your AI dynamically create the payload.
     payload = {
-        # "question": "Buy me a pair of shoes",
-        # "shoe_size": 12,
-        # "favorite_color": "black",
-        # "hello": "puerto rico",
-        "id": "sadfsadfsaf",
-        "session": "",
+        "question": "Buy me a pair of shoes",
+        "shoe_size": 12,
+        "favorite_color": "black",
     }
 
     # Send your message and include your AI's identity
     # to enable dialogue between your AI and the
-    # one sending the request to.
+    # one you are sending the request to.
     send_message_to_agent(
         sender_identity,
         AGENT_ADDRESS,
