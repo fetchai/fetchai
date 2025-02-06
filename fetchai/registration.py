253e1c6dca01f08a22e8ffaf4f3146399a3cffb5
zmezei <mezeienator@gmail.com>
2025-02-06 17:55:08 +0000

    revert removal of register_with_agentverse almanac_api arg

--- fetchai/registration.py
+++ fetchai/registration.py
@@ -1,3 +1,6 @@
+from typing import Optional
+from urllib.parse import urlparse
+from uagents_core.config import AgentverseConfig
 from uagents_core.crypto import Identity
 from uagents_core.registration import AgentverseConnectRequest, AgentUpdates
 from uagents_core.types import AgentType
@@ -12,6 +15,7 @@ def register_with_agentverse(
     readme: str,
     *,
     protocol_digest: str = "proto:a03398ea81d7aaaf67e72940937676eae0d019f8e1d8b5efbadfef9fd2e98bb2",
+    almanac_api: Optional[str] = None,
     agent_type: AgentType = "custom",
 ):
     """
@@ -22,8 +26,20 @@ def register_with_agentverse(
     :param agent_title: The title of the agent
     :param readme: The readme for the agent
     :param protocol_digest: The digest of the protocol that the agent supports
+    :param almanac_api: The URL of the Almanac API (if different from the default)
     :return:
     """
+
+    if almanac_api:
+        almanac_api_parsed = urlparse(almanac_api)
+        protocol = almanac_api_parsed.scheme
+        base_url = f"{almanac_api_parsed.netloc}{almanac_api_parsed.path}"
+
+        agentverse_config = AgentverseConfig(
+            base_url=base_url, protocol=protocol, http_prefix=protocol
+        )
+    else:
+        agentverse_config = AgentverseConfig()
 
     agentverse_connect_request = AgentverseConnectRequest(
         user_token=agentverse_token,
@@ -35,6 +51,7 @@ def register_with_agentverse(
         request=agentverse_connect_request,
         identity=identity,
         protocol_digests=[protocol_digest],
+        agentverse_config=agentverse_config,
     )
 
     agent_updates = AgentUpdates(name=agent_title, readme=readme)
@@ -42,4 +59,5 @@ def register_with_agentverse(
         request=agentverse_connect_request,
         identity=identity,
         agent_details=agent_updates,
+        agentverse_config=agentverse_config,
     )
